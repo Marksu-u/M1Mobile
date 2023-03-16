87b5b139f52a103b8a8fd3477b5bd568744576e3
Marksu-u <72389013+Marksu-u@users.noreply.github.com>
2023-03-16 17:00:15 +0100

    navigator start

--- YourFitnessBuddy/src/components/appNavigator/index.js
+++ YourFitnessBuddy/src/components/appNavigator/index.js
@@ -1,43 +1,11 @@
-import React from 'react';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { NavigationContainer } from '@react-navigation/native';
+import React from "react";
+import { Text, View } from "react-native";
 
-import HomeScreen from '../../screens/home';
-import ExercisesScreen from '../../screens/exercises';
-import FavoritesScreen from '../../screens/favorites';
 
-import { Ionicons } from '@expo/vector-icons';
-
-const Tab = createBottomTabNavigator();
-
-const AppNavigator = () => {
-  return (
-    <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color, size }) => {
-            let iconName;
-
-            if (route.name === 'Home') {
-              iconName = focused ? 'home-sharp' : 'home-outline';
-            } else if (route.name === 'Exercises') {
-              iconName = focused ? 'barbell-sharp' : 'barbell-outline';
-            } else if (route.name === 'Favorites') {
-              iconName = focused ? 'heart-sharp' : 'heart-outline';
-            }
-
-            return <Ionicons name={iconName} size={size} color={color} />;
-          },
-          tabBarActiveTintColor: 'tomato',
-          tabBarInactiveTintColor: 'gray',
-        })}
-      >
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Exercises" component={ExercisesScreen} />
-        <Tab.Screen name="Favorites" component={FavoritesScreen} />
-      </Tab.Navigator>
-    </NavigationContainer>
-  );
-};
+const AppNavigator = () => (
+    <View>
+        <Text>Navigator</Text>
+    </View>
+);
 
 export default AppNavigator;
